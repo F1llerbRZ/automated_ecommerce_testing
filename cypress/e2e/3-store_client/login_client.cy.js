7008e1455fffb869f676f4145ce6e6dbbe9c638b
F1llerbRZ <fillerbrz@gmail.com>
2024-06-18 01:43:13 -0300

    second update - negative scenario

--- cypress/e2e/3-store_client/login_client.cy.js
+++ cypress/e2e/3-store_client/login_client.cy.js
@@ -8,5 +8,16 @@ describe('Functionality = Login', () => {
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, thiago_test (não é thiago_test? Sair)')
+
+    });
+
+    it('Should display an error message when entering invalid user', () => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/') 
+        cy.get('#username').type('thiago@testmail.com')
+        cy.get('#password').type('09038512aB')   
+        cy.get('.woocommerce-form > .button').click()
+
+        cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido. Verifique novamente ou tente seu nome de usuário.')
+
     });
 });
