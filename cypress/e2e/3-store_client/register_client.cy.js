b2078a6f3d6b550b2d89f160befd55f04e9092d7
F1llerbRZ <fillerbrz@gmail.com>
2024-06-24 01:14:38 -0300

    Using faker and variables

---
+++ cypress/e2e/3-store_client/register_client.cy.js
@@ -0,0 +1,48 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
+
+describe('Functionality = Register', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+    it('Must complete registration successfully', () => {
+        cy.get('#reg_email').type(faker.internet.email())
+        cy.get('#reg_password').type('09038512aB')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+
+        cy.get('#account_first_name').type(faker.person.firstName())
+        cy.get('#account_last_name').type(faker.person.lastName())
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
+    });
+
+    it('Must complete registration successfully - Using variables', () => {
+
+        var email = faker.internet.email()
+        var nome = faker.person.firstName()
+        var sobrenome = faker.person.lastName()
+
+        cy.get('#reg_email').type(email)
+        cy.get('#reg_password').type('09038512aB')
+        cy.get(':nth-child(4) > .button').click()
+
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('exist')
+
+        cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+
+        cy.get('#account_first_name').type(nome)
+        cy.get('#account_last_name').type(sobrenome)
+        cy.get('.woocommerce-Button').click()
+        cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+
+    });
+    
+});
